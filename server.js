aae2f83ae131446e7a1370d969e109ac589e0098
bchsmanikanta-tech <b.chs.manikanta@gmail.com>
2026-07-14 20:09:01 +0530

    fix: increase json payload size limit to 15mb

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '15mb', extended: true }));
 
 // Paths
 const UPLOADS_DIR = path.join(__dirname, 'uploads');
