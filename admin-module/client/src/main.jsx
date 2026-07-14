1554df87a3b328a459d8f7aa6b12941601cc73a0
bchsmanikanta-tech <b.chs.manikanta@gmail.com>
2026-07-14 17:31:24 +0530

    feat: make admin API base URL configurable for separate domain deployment

--- admin-module/client/src/main.jsx
+++ admin-module/client/src/main.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './index.css';
 import App from './App.jsx';
 
-axios.defaults.baseURL = '/';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || '/';
 axios.interceptors.request.use((config) => {
   if (config.url?.startsWith('http://localhost:5000')) {
     config.url = config.url.replace('http://localhost:5000', '');
